359b982f5f84cc74e9e2b91110cab90fed052c71
Aleksei <spacerabbit1@mail.ru>
2022-09-27 22:58:24 +0700

    end

---
+++ task29/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Задайте длинну массива: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[number];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 100);
+}
+PrintArray(array);
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if(i < array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.Write("]");
+}
